062472d331f322621c4911f1e931a8a3444379f5
Topan <topan@altonium.com>
2023-04-23 17:44:39 +0700

    add img for tic tac toe

---
+++ test-array.js
@@ -0,0 +1,16 @@
+//ARRAY
+let jambu=7;
+// single dimension array
+let board=[1,2,3];
+let board3=["semangka",3,"mangga"];
+
+//multi dimension array
+
+let board2=[
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+];
+
+// console.log(board[2]*jambu);
+console.log(board2[2][1]);
